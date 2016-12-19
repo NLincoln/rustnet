54d374f40050eb213083ed770605a320f17c11a4
nlincoln <nlincoln@intellifarms.com>
2016-12-18 20:15:55 -0600

    Simple networking base. Always returns hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{ BufReader, BufRead, Read, Write };
+use std::net::{ TcpListener };
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4001").unwrap();
+
+    for stream in listener.incoming() {
+        thread::spawn(|| {
+            let mut reader = BufReader::new(stream.unwrap());
+            for line in reader.by_ref().lines() {
+                if line.unwrap() == "" {
+                    break;
+                }
+            }
+            reader.into_inner().write_all(b"HTTP/1.1 200 OK\n\nHello World").unwrap();
+        });
+    }
 }
